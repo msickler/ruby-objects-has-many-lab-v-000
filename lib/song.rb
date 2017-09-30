14da64a950884baef32638595c2963f9f25ea8aa
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-30 19:56:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,11 @@ require_relative "artist.rb"
 class Song
   attr_accessor :name, :artist
 
-  def initialize
+  def initialize(name)
     @name = name
+  end
+
+  def artist
     @artist = Artist.new
   end
 end
