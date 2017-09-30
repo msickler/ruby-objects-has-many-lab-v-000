17fd98f25bd1b0d2283eb98a9b09ba5e236fb697
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-30 20:12:11 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,18 @@
+class Author
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @post = Post.new(post)
+    post.author = self
+  end
+
+end
