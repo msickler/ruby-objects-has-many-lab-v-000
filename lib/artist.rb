9ad2afcde2e46bb48adf796d3d14b867604908a2
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-30 20:09:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,7 @@ require_relative "song.rb"
 
 class Artist
  attr_accessor :name
+ @@song_count = 0
 
 def initialize(name)
   @name = name
@@ -15,12 +16,18 @@ end
 def add_song(song)
   @songs << song
   song.artist = self
+  @@song_count += 1
 end
 
 def add_song_by_name(name)
   song = Song.new(name)
   @songs << song
   song.artist = self
-
+  @@song_count +=1
 end
+
+def self.song_count
+  @@song_count
+end
+
 end
