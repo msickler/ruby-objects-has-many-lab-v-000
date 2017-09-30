d8ff96e037a8a6e45a91accb53ba7ce8e417cb48
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-30 20:26:36 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,7 @@ require_relative "post.rb"
 
 class Author
   attr_accessor :name, :posts
+  @@post_count = 0
 
   def initialize(name)
     @name = name
@@ -15,12 +16,17 @@ class Author
   def add_post(posts)
     @posts << posts
     posts.author = self
+    @@post_count += 1
   end
 
 def add_post_by_title(title)
   posts = Post.new(title)
   posts.author = self
   @posts << posts
+  @@post_count += 1
 end
 
+def post_count
+ @@post_count
+end
 end
