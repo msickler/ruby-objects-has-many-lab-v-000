869510907024591e1dbd0f511f51aa117e6b93c7
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-30 20:15:20 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,5 +1,24 @@
 require_relative "author.rb"
 
 class Post
+ attr_accessor :title
 
+ def initialize(title)
+   @title = title
+ end
+
+ def title
+   @title
+ end
+
+ def author
+   author = Author.new
+ end
+
+ def author_name
+   if @title.author == nil
+     nil
+   else @title.author = author
+   end
+end
 end
